ebd4374ca04fde4a1d9704aafc0a7be52820366a
Maksim Makarenko <makmakale87@gmail.com>
2024-04-08 20:26:25 +0300

    added Status: page, model, actions

--- app/(protected)/statuses/page.jsx
+++ app/(protected)/statuses/page.jsx
@@ -1,9 +1,36 @@
-export default async function StatusesPage(props) {
-  return (
-    <div className={'w-full h-full flex justify-center items-center p-10'}>
-      <h1 className={"text-3xl md:text-7xl mb-10 text-center font-black"}>
-        Statuses
-      </h1>
-    </div>
-  )
+'use client'
+import {IsDefault} from "@/components/Views/Table/components/Columns/is-default";
+import DeleteAction from "@/components/Views/Table/components/Columns/delete-action";
+import TableView from "@/components/Views/Table";
+import {deleteStatus, getListOfStatuses, setDefaultStatus} from "@/lib/actions/statuses";
+
+export default function StatusesPage(props) {
+  const columns = [
+    {id: 'id', title: 'ID', className: 'w-[40px]', align: 'center'},
+    {id: 'title', title: 'Title', link: '/projects/edit'},
+    {id: 'description', title: 'Description'},
+    {
+      id: 'isDefault',
+      title: 'Default',
+      className: 'w-[80px]',
+      align: 'center',
+      renderValue: IsDefault,
+    },
+    {
+      id: 'delete',
+      title: '',
+      className: 'w-[40px]',
+      align: 'center',
+      renderValue: DeleteAction
+    },
+  ]
+
+  return <TableView
+    title={"Statuses"}
+    searchLabel={"Search status..."}
+    columns={columns}
+    getData={getListOfStatuses}
+    setDefault={setDefaultStatus}
+    onDelete={deleteStatus}
+  />
 }
